1a666dc11d1c31c2c122e3a7a2c1060de73e5c4a
Nicolau <nicolau.riess@lumx.io>
2024-02-26 15:03:28 -0300

    initial commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,33 @@
-'use client';
+"use client";
 
-import { WalletContextProvider } from '@lumx-protocol/embedded-wallet';
-import { Button } from './ui/button';
-import Image from 'next/image';
-import { Profile } from './profile';
-import { useState } from 'react';
+import { WalletContextProvider } from "@lumx-protocol/embedded-wallet";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { Profile } from "./profile";
+import { useState } from "react";
 
 export const Header = () => {
-	const [user, setUser] = useState<{ walletAddress: string; name: string }>(
-		JSON.parse(window.localStorage.getItem('wallet.user') || '{}')
-	);
+  const [user, setUser] = useState<{ walletAddress: string; name: string }>(
+    JSON.parse(window.localStorage.getItem("wallet.user") || "{}")
+  );
 
-	return (
-		<WalletContextProvider
-			clientId='cb63683f-c98b-4ac4-8dfc-4692daaab6a0'
-			isModal
-			environment='sandbox'
-			onFinishAuth={(user) => {
-				setUser(user as { walletAddress: string; name: string });
-			}}
-		>
-			<header className='flex gap-4 pt-12 items-center sm:px-[calc(100%-85vw)] px-[calc(5vw)]'>
-				<Image src='./lumx-logo.svg' height={39} width={39} alt='lumx logo' />
-				<Button className='w-fit mr-auto hidden sm:block' variant={'secondary'}>
-					Welcome
-				</Button>
-				<Profile {...user} />
-			</header>
-		</WalletContextProvider>
-	);
+  return (
+    <WalletContextProvider
+      clientId="cb63683f-c98b-4ac4-8dfc-4692daaab6a0"
+      isModal
+      environment="sandbox"
+      onFinishAuth={(user) => {
+        setUser(user as { walletAddress: string; name: string });
+      }}
+      theme="system"
+    >
+      <header className="flex gap-4 pt-12 items-center sm:px-[calc(100%-85vw)] px-[calc(5vw)]">
+        <Image src="./lumx-logo.svg" height={39} width={39} alt="lumx logo" />
+        <Button className="w-fit mr-auto hidden sm:block" variant={"secondary"}>
+          Welcome
+        </Button>
+        <Profile {...user} />
+      </header>
+    </WalletContextProvider>
+  );
 };
